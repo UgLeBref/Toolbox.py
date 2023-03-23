408aceac2b6f7f96bb8b57995da066bebf6435b4
UgLeBref <96297483+UgLeBref@users.noreply.github.com>
2023-03-23 16:23:10 +0100

    Add files via upload

---
+++ Info.py
@@ -0,0 +1,32 @@
+import os
+
+os.system("clear")
+
+print("1. Détection des hôtes.")
+print("2. Scan des ports d'un hôte.")
+print("3. Scan complet d'un hôte.")
+print("0. Menu principal.")
+
+menu = input("Collecte d'information : ")
+
+if menu == '1':
+    os.system("clear")
+    ip_add = input("Saisir l'adresse ip du réseau : ")
+    mask = input("Saisir le masque réseau : ")
+    os.system(f"nmap -sn {ip_add}/{mask}")
+
+elif menu == '2':
+    os.system("clear")
+    ip_add = input("Saisir l'adresse ip cible : ")
+    os.system(f"nmap {ip_add}")
+
+elif menu == '3':
+    os.system("clear")
+    ip_add = input("Saisir l'adresse ip cible : ")
+    os.system(f"nmap -A -O -sV -sC -T4 -oN nmapoutput {ip_add}")
+
+elif menu == '0':
+    os.system("python main.py")
+
+else:
+    print("\n Erreur : la saisie est incorrecte.")
---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+
+os.system("clear")
+
+# Afficher le menu
+print("Menu :")
+print("1. Collecte d'information.")
+print("2. Scan des vulérabilités.")
+print("3. ")
+print("4. ")
+print("5 . Arreter le programme.")
+choix = input("Saisir l'option : ")
+
+if choix == "1":
+    os.system("python3 Info.py")
+
+if choix == "2":
+    ip_add = input("Saisir l'adresse ip du réseau : ")
+    os.system(f"nmap -sV --script=vulscan/ {ip_add}")
+
+elif choix == "5":
+    print("Fin du programme.")
+
+else:
+    print("Choix invalide")
+
+input("Continuer")
