a06592182154e4ecf13e39bd6c9ab6991dfc34f8
UgLeBref <96297483+UgLeBref@users.noreply.github.com>
2023-03-24 07:19:25 -0400

    Add files via upload

--- Dorks.py
+++ Dorks.py
@@ -19,35 +19,35 @@ cible = input("Quel est le domaine cible : ")
 
 if menu == '1':
     os.system("clear")
-    os.system(f"python pagodo/pagodo.py -d {cible} -g all_google_dorks.txt")
+    os.system(f"python pagodo/pagodo.py -s -d {cible} -g pagodo/dorks/all_google_dorks.txt")
 
 elif menu == '2':
     os.system("clear")
-    os.system(f"python pagodo/pagodo.py -d {cible} -g files_containing_passwords.dorks")
+    os.system(f"python pagodo/pagodo.py -s -d {cible} -g pagodo/dorks/files_containing_passwords.dorks")
 
 elif menu == '3':
     os.system("clear")
-    os.system(f"python pagodo/pagodo.py -d {cible} -g files_containing_usernames.dorks")
+    os.system(f"python pagodo/pagodo.py -s -d {cible} -g pagodo/dorks/files_containing_usernames.dorks")
 
 elif menu == '4':
     os.system("clear")
-    os.system(f"python pagodo/pagodo.py -d {cible} -g sensitive_online_shopping_info.dorks")
+    os.system(f"python pagodo/pagodo.py -s -d {cible} -g pagodo/dorks/sensitive_online_shopping_info.dorks")
 
 elif menu == '5':
     os.system("clear")
-    os.system(f"python pagodo/pagodo.py -d {cible} -g pages_containing_login_portals.dorks")
+    os.system(f"python pagodo/pagodo.py -s -d {cible} -g pagodo/dorks/pages_containing_login_portals.dorks")
 
 elif menu == '6':
     os.system("clear")
-    os.system(f"python pagodo/pagodo.py -d {cible} -g vulnerable_files.dorks")
+    os.system(f"python pagodo/pagodo.py -s -d {cible} -g pagodo/dorks/vulnerable_files.dorks")
 
 elif menu == '7':
     os.system("clear")
-    os.system(f"python pagodo/pagodo.py -d {cible} -g vulnerable_servers.dorks")
+    os.system(f"python pagodo/pagodo.py -s -d {cible} -g pagodo/dorks/vulnerable_servers.dorks")
 
 elif menu == '8':
     os.system("clear")
-    os.system("python pagodo/ghdb_scrapper.py")
+    os.system("python pagodo/ghdb_scrapper.py -s -j -i")
 
 elif menu == '0':
     os.system("python main.py")
@@ -56,3 +56,4 @@ else:
     print("\n Erreur : la saisie est incorrecte.")
 
 input("Continuer")
+os.system("python Dorks.py")
